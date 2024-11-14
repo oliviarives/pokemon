9d0fd0c667e6c6969cab3d5606fc770edff6df38
oliviarives <oliviamazone@gmail.com>
2024-11-14 16:47:18 +0100

    ajout de test dans le main

--- src/pokemonNormaux/Tests.java
+++ src/pokemonNormaux/Tests.java
@@ -1,10 +1,36 @@
 package pokemonNormaux;
 
+import java.util.Random;
+
+import pokemonsNormaux.Pokemon0;
+
 public class Tests {
 
-	public static void main(String[] args) {
-		System.out.println("test");
+	private static java.util.Random random = new Random();
 
+	public static void main(String[] args) {
+		int min = 10;
+		int max = 0;
+		// 20 Lancers de 1D6
+		for (int i = 1; i <= 20; i++) {
+			int r = random.nextInt(1, 7);
+			System.out.println(r +" ");
+			if(r < min)
+				min = r;
+			else if (r > max)
+				max = r;	
+		}
+		System.out.println("\n["+ min + "," + max + "]");
+		
+		System.out.println("\n");
+		System.out.println("\nTEST POKEMON0 \n");
+		Pokemon0 p1 = new Pokemon0("Rhinolove");
+		Pokemon0 p2 = new Pokemon0("Chovsourir");
+		p1.log(p1.toString());
+		p2.log(p2.toString());
+		p1.attaquer(p2);
+		p2.log(p2.toString());
+		
 	}
 
 }
